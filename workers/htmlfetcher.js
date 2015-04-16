1d1a0baf3aa1bcfea9a80187caf8f2436c1f2bce
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-16 12:21:38 -0500

    log errors in logerrors.txt

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,9 +1,11 @@
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
 var helpers = require('../helpers/archive-helpers.js');
+var fs = require('fs');
 
 process.on('uncaughtException', function (err) {
     console.log(err);
+    fs.appendFile('logerrors.txt', new Date() + ': ' + err + '\n\n\n');
 });
 
 helpers.readListOfUrls(function(data) {
