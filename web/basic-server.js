ef06220e5490e32db2e2d5eb1e7204855990fc55
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-20 10:48:56 -0500

    add process.env.IP

--- web/basic-server.js
+++ web/basic-server.js
@@ -7,7 +7,7 @@ var initialize = require("./initialize.js");
 initialize();
 
 var port = process.env.PORT || 8080;
-var ip = "127.0.0.1";
+var ip = process.env.IP || "127.0.0.1";
 var server = http.createServer(handler.handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
